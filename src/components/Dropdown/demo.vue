1a9d65974a3d9fba9014dadb4447cbb0f2dfac0f
FatGe <duangci@aliyun.com>
2019-03-31 11:49:03 +0800

    fix(fix dropdown examples):

--- src/components/Dropdown/demo.vue
+++ src/components/Dropdown/demo.vue
@@ -39,7 +39,7 @@
     <Demo-row
       title="触发方式"
       description="可以配置 click 激活或者 hover 激活"
-      :code="example1"
+      :code="example2"
     >
       <fat-dropdown trigger="click">
         <template slot="menu">
@@ -71,7 +71,7 @@
     <Demo-row
       title="动态生成自定义下拉列表"
       description="可以通过配置不同插槽，来修改Dropdown的label、menu模块"
-      :code="example1"
+      :code="example3"
     >
       <fat-dropdown v-model="selectValue" option-key="label" trigger="click">
         <template slot="label">
@@ -94,7 +94,7 @@
     <Demo-row
       title="自定义 Option 事件"
       description="可以修改每个 option 的点击事件"
-      :code="example1"
+      :code="example4"
     >
       <fat-dropdown v-model="selectValue" option-key="label" trigger="click">
         <template slot="menu">
@@ -149,35 +149,88 @@ export default {
   },
   computed: {
     example1() {
-      return `<fat-button class="btn" size="mini">超小按钮</fat-button>
-<fat-button class="btn" size="small">小型按钮</fat-button>
-<fat-button class="btn" size="medium">默认按钮</fat-button>
-<fat-button class="btn" size="large">大型按钮</fat-button>`;
+      return `<fat-dropdown v-model="selectValue">
+    <template slot="menu">
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://fatge.xyz/"
+            >主页</a
+        >
+        </fat-dropdown-option>
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://github.com/FatGe"
+            >Github</a
+        >
+        </fat-dropdown-option>
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://juejin.im/user/5b3ed96c6fb9a04fc34bed91"
+            >掘金</a
+        >
+        </fat-dropdown-option>
+    </template>
+</fat-dropdown>`;
     },
     example2() {
-      return `<fat-button class="btn">
-    <fat-icon name="done"/>确定
-</fat-button>
-<fat-button class="btn">
-    <fat-icon name="pets"/>确定
-</fat-button>
-<fat-button class="btn">
-    <fat-icon name="search"/>确定
-</fat-button>`;
+      return `<fat-dropdown trigger="click">
+    <template slot="menu">
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://fatge.xyz/"
+            >主页</a
+        >
+        </fat-dropdown-option>
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://github.com/FatGe"
+            >Github</a
+        >
+        </fat-dropdown-option>
+        <fat-dropdown-option>
+        <a
+            class="c-color-normal t-none-decoration"
+            href="https://juejin.im/user/5b3ed96c6fb9a04fc34bed91"
+            >掘金</a
+        >
+        </fat-dropdown-option>
+    </template>
+</fat-dropdown>`;
     },
     example3() {
-      return `<fat-button class="btn" type="primary" @click="handleClick">primary</fat-button>
-<fat-button class="btn" type="success">success</fat-button>
-<fat-button class="btn" type="warning">warning</fat-button>
-<fat-button class="btn" type="error">error</fat-button>
-<fat-button class="btn" type="text">文字按钮</fat-button>`;
+      return `<fat-dropdown v-model="selectValue" option-key="label" trigger="click">
+    <template slot="label">
+        <span class="c-color-success">{{ selectValue }}</span>
+    </template>
+
+    <template slot="menu">
+        <fat-dropdown-option
+        v-for="(option, index) in options"
+        :key="index"
+        :label="option.label"
+        :value="option.value"
+        >
+        {{ option.label }}
+        </fat-dropdown-option>
+    </template>
+</fat-dropdown>`;
     },
     example4() {
-      return `<fat-button class="btn" type="primary" disabled>primary</fat-button>
-<fat-button class="btn" type="success" disabled>success</fat-button>
-<fat-button class="btn" type="warning" disabled>warning</fat-button>
-<fat-button class="btn" type="error" disabled>error</fat-button>
-<fat-button class="btn" type="text" disabled>文字按钮</fat-button>`;
+      return `<fat-dropdown v-model="selectValue" option-key="label" trigger="click">
+    <template slot="menu">
+        <fat-dropdown-option>
+        <span @click="handleClick">警告：小心点</span>
+        </fat-dropdown-option>
+        <fat-dropdown-option disabled>
+        禁止点击
+        </fat-dropdown-option>
+    </template>
+</fat-dropdown>`;
     }
   },
   methods: {
